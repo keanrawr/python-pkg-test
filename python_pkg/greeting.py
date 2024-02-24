9c9c7334993fb5aa4d88f76a491fde130cb2cd07
keanrawr <git@keanrawr.com>
2024-02-23 21:17:55 -0600

    Add docstrings to greeting module

--- python_pkg/greeting.py
+++ python_pkg/greeting.py
@@ -1,5 +1,13 @@
 
 def say_hello(name=None):
+    """
+    Function that returns a simple greeting starting with "Hello"
+    an optional `name` parameter can be provided to personalize
+    the greeting
+
+    :param name: optional name to personalize greeting
+    :type name: str
+    """
     if name is not None:
         return f'Hello {name}!'
 
@@ -7,6 +15,14 @@ def say_hello(name=None):
 
 
 def say_hi(name=None):
+    """
+    Function that returns a simple greeting starting with "Hi"
+    an optional `name` parameter can be provided to personalize
+    the greeting
+
+    :param name: optional name to personalize greeting
+    :type name: str
+    """
     if name is not None:
         return f'Hi {name}!'
 
